1de664ce2d6431d82c427b00fac53cf28cbdf6a6
Angel David Rojas Riveros <halkdavid190@gmail.com>
2024-10-25 10:52:09 -0500

    ConsumoAPI
    se agrega el consumo de un api mediante una clase de java que llamamos a un metodo que tiene el cual solicita la url y nos retorna un string que seria el json que genera esa api

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,22 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println("Hola Mundo en Spring");
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=839d3133");
+		System.out.println(json);
+	}
 }
